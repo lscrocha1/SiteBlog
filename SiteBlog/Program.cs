da69a343efa1b30a756d2a44842aa6d6c1077112
Lucas Rocha <dev.lucascrocha@gmail.com>
2022-02-12 14:29:35 -0300

    Adds swagger

--- SiteBlog/Program.cs
+++ SiteBlog/Program.cs
@@ -2,9 +2,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
+services.AddEndpointsApiExplorer();
+
+services.AddSwaggerGen();
+
 services.AddControllers();
 
 var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(opts =>
+{
+    opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    opts.RoutePrefix = string.Empty;
+});
 
 app.MapControllers();
 
